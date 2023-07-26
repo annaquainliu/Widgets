fe8455fbf6dd9b17112ed2f0a152688dd7a3c7c0
Anna Liu <annaquainliu@gmail.com>
2023-07-26 14:52:47 -0700

    Initial Commit

---
+++ Widgets/WidgetsApp.swift
@@ -0,0 +1,17 @@
+//
+//  WidgetsApp.swift
+//  Widgets
+//
+//  Created by Anna Liu on 7/26/23.
+//
+
+import SwiftUI
+
+@main
+struct WidgetsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
